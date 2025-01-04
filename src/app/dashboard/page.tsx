5a1b44a278851ecb6d72727fa8c2b19ce288292f
neutr0n420 <aryan.bramhane@ortigan.in>
2025-01-04 21:58:21 +0530

    added redem funds feature for teacher

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,12 +7,12 @@ import { classroomABI } from "@/src/utils/constants";
 import { Table, TableBody, TableCell, TableRow, TableHead, TableHeader } from "@/src/components/ui/table"
 
 const Page = () => {
-  const [classroomAddress, setClassroomAddress] = React.useState<string[]>([]);
   interface StudentData {
     students: string[];
     timestamps: string[];
   }
-
+  const [classroomAddress, setClassroomAddress] = React.useState<string[]>([]);
+  const [totalEnrolledStudents, setTotalEnrolledStudents] = useState<number>(0);
   const [studentData, setStudentData] = useState<StudentData>({ students: [], timestamps: [] });
 
   // const [studentData, setStudentData] = React.useState<Student[]>([]);
@@ -21,7 +21,6 @@ const Page = () => {
   const { isConnected } = useAppKitAccount();
   useEffect(() => {
     FactoryContractInstance().then((res) => {
-      console.log(res?.classroomAddresses);
       setClassroomAddress(res?.classroomAddresses);
     });
 
@@ -30,33 +29,34 @@ const Page = () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum as ethers.providers.ExternalProvider);
         const signer = provider.getSigner();
         const ClassOwner = await signer.getAddress();
-        console.log(ClassOwner);
         if (ClassOwner.toLowerCase() === (await signer.getAddress()).toLowerCase()) {
           classroomAddress.map(async (classAddress) => {
             const ClassroomContract = new ethers.Contract(classAddress, classroomABI, signer);
             const studentDetails = await ClassroomContract.getStudentsWithTimestamps();
-            // const studentData = { studentDetails.students, studentDetails.timestamps };
-            console.log("This is student details with timeStamp", studentDetails.students);
             setStudentData(studentDetails);
+            const studentCount = await ClassroomContract.getTotalEnrolledStudents()
+            console.log("Student Count", Number(studentCount._hex));
+            setTotalEnrolledStudents(Number(studentCount._hex));
             return studentDetails;
           })
         }
       }
     }
+
     getStudentDetails()
-  }, [classroomAddress, isConnected])
+  }, [isConnected, classroomAddress]);
+
 
   const formatTimestamp = (timestamp: string) => {
     if (!timestamp) return "-";
-    // Convert BigNumber to number and then to Date
     const date = new Date(parseInt(timestamp) * 1000);
     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
   };
 
-  console.log("This is state variable", studentData);
   // getStudentDetails()
   return (
     <div className="text-white">
+      <div className="bg-indigo-500 w-1/5 text-center mx-8 rounded-xl">Total Enrolled Student: {totalEnrolledStudents}</div>
       <Table>
         <TableHeader>
           <TableRow>
@@ -68,7 +68,6 @@ const Page = () => {
         <TableBody>
           {/* <TableHead>Enrolled Student Data</TableHead> */}
           {studentData !== undefined && studentData.students !== undefined && studentData.students.length > 0 && studentData.students.map((student, index) => (
-            console.log("This is student", student),
             <TableRow key={index}>
               <TableCell>{index + 1}</TableCell>
               <TableCell>
