cfe947ed835cf82cbbe0b33414f57588397c1135
neutr0n420 <aryan.bramhane@ortigan.in>
2025-01-05 16:48:07 +0530

    video-calling working

--- src/app/dashboard/video-call/[roomId]/page.tsx
+++ src/app/dashboard/video-call/[roomId]/page.tsx
@@ -1,20 +1,51 @@
 "use client"
 import { Video, Mic } from "lucide-react";
-import { useLocalVideo, useLocalAudio } from "@huddle01/react"
+import { useLocalVideo, useLocalAudio, useRoom } from "@huddle01/react"
+import { useRouter } from "next/navigation";
 import { useAccessStore } from "@/src/store/store";
-import { useEffect, useRef } from "react";
-const Page = () => {
+import { use, useEffect, useRef } from "react";
+interface PageProps {
+    params: {
+        roomId: string;
+    };
+}
+const Page = ({ params }: PageProps) => {
     const { stream, enableVideo, disableVideo, isVideoOn } = useLocalVideo();
+    const { joinRoom, state } = useRoom({
+        onJoin: () => {
+            console.log("Joined Room");
+        },
+    })
     const { enableAudio, disableAudio, isAudioOn } = useLocalAudio();
     const videoRef = useRef<HTMLVideoElement>(null);
     const setAccessToken = useAccessStore((state) => state.setAccessToken);
+    const router = useRouter();
+    //@ts-expect-error as the unwrapped params are used as string
+    const unwrappedParams = use(params as { roomId: string });
     useEffect(() => {
         console.log("stream", stream);
         if (stream && videoRef.current) {
             videoRef.current.srcObject = stream;
         }
         setAccessToken("token")
+        console.log("accessToken");
     }, [stream]);
+    const handleJoin = async () => {
+        console.log("join");
+        //@ts-expect-error as the unwrapped params are used as string
+        const createToken = await fetch(`http://localhost:3000/api/huddle-route?roomId=${unwrappedParams.roomId}`);
+        const accessToken = await createToken.text();
+        localStorage.setItem("teacher-access-token", JSON.stringify(accessToken));
+        joinRoom({
+            //@ts-expect-error as the unwrapped params are used as string
+            roomId: unwrappedParams.roomId,
+            token: accessToken
+        });
+        console.log("joined");
+        //@ts-expect-error as the unwrapped params are used as string
+        router.push(`/dashboard/video-call/${unwrappedParams.roomId}/join`);
+
+    }
     return (
         <div className="text-white flex items-start justify-center my-20">
             <div className="w-1/2">
@@ -32,7 +63,7 @@ const Page = () => {
             <div className="flex-col ml-12 justify-evenly text-center mt-24">
                 {/* <h1 className="text-4xl font-extrabold mb-6" onClick={ }>Ready to Join? </h1> */}
                 <h4 className="my-4">No one else in here</h4>
-                <button className="bg-gradient-to-r from-purple-400 px-16 to-pink-600 py-4 rounded-lg">join now</button>
+                <button className="bg-gradient-to-r from-purple-400 px-16 to-pink-600 py-4 rounded-lg" onClick={() => handleJoin()}>{state === "connecting" ? (<div>joining...</div>) : <div>join now</div>}</button>
             </div>
         </div>
 
